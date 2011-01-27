cc23288466a45d352f2895677eaad814b7196ace
Aaron Rustad <arustad@Aaron-Rustads-MacBook-Pro.local>
2011-01-26 18:39:41 -0700

    refactored code

--- todo.js
+++ todo.js
@@ -1,45 +1,28 @@
 var http = require('http');
 var fs  = require('fs');
 
-function saveData() {
-  var map = {};
-  fs.readFile('todo.txt', function(err, data) {
-    if (err)  {
-        throw err;
-    }
-    if (data) {
-      map = JSON.parse(data);
-    }
-  });
-  console.log(map.house);
-  return map;
-}
+function addTask(params, callback) {
+     fs.readFile('todo.txt', function(err, data) {
+       if (err) {
+         throw err;
+       }
+       var dataMap = {};
+       if(data.length !== 0) {
+         dataMap = JSON.parse(data);
+       }
+       var groupName = params.group;
 
-function saveFileAsJSON(map) {
-    fs.writeFile('todo.txt', JSON.stringify(map), function(err) {
-      if (err) {
-        throw err;
-      }
-    });
-}
+       var group = dataMap[groupName];
+       if (! group) {
+            group = [];
+            dataMap[groupName] = group;
+       }
+       dataMap[groupName][group.length] = params.task;
 
-function addTask(params) {
-  var dataMap = readFileAsMap();
-  
-  var groupName = params.group;
-
-  var group = dataMap[groupName];
-
-  if (! group) {
-    group = [];
-    dataMap[groupName] = group;
-  }
-  dataMap[groupName][group.length] = params.task;
-
-  saveFileAsJSON(dataMap);
-}
-
-function removeTask(message) {
+       result = JSON.stringify(dataMap);
+       fs.writeFileSync('todo.txt', result);
+       callback(data);
+     });
 }
 
 var actionFunctions = {
@@ -47,13 +30,17 @@ var actionFunctions = {
 };
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Message Received');
-
     var params = require('url').parse(req.url, true);
     var action = params.pathname.slice(1);
-    console.log(actionFunctions);
-    actionFunctions[action](params.query);
+
+    var data = null;
+    if (params.query) {
+      actionFunctions[action](params.query, function(data) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(data);
+      });
+    }
+
 
 }).listen(8124, "127.0.0.1");
 
